a4f6f7015c544665e1718ccdc38bbedc27e0c060
Alex Cardenas <alexandercardenas50x@gmail.com>
2025-10-24 12:34:05 -0500

    Culminacion del ejercicio 2

--- auth-service/users/views.py
+++ auth-service/users/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.views import APIView
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -15,7 +16,7 @@ class RegisterView(generics.CreateAPIView):
             return Response({"message": "Usuario creado correctamente"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class MeView(generics.RetrieveAPIView):
+class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
